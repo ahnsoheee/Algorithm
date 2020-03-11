625e8458fda1aa78061ce0af2f7c954db960d6b9
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-03-12 00:29:14 +0900

    20.03.11
    파도반 수열

---
+++ Baekjoon/9461.py
@@ -0,0 +1,13 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(0, T):
+    N = int(sys.stdin.readline())
+    seq = [1, 1, 1, 2, 2]
+    
+    if N > 5:
+        for j in range(5, N):
+            seq.append(seq[j-1] + seq[j-5])
+    
+    print(seq[N-1])
