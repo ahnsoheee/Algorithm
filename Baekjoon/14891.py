c106edd8ad0417f22188fca7c12928e1a4a47674
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-07-02 03:30:42 +0900

    20.07.01

--- Baekjoon/14891.py
+++ Baekjoon/14891.py
@@ -1,2 +1,45 @@
 #No. 14891 '톱니바퀴'
 
+import sys, collections
+s = []
+for _ in range(4):
+    s.append(collections.deque(list(input())))
+
+
+K = int(sys.stdin.readline())
+R = [list(map(int, sys.stdin.readline().split())) for _ in range(K)]
+
+def left(num, direction): #왼쪽 톱니바퀴 확인
+    if num < 0:
+        return 
+    if s[num][2] != s[num+1][6]:
+        left(num-1, -direction)
+        s[num].rotate(direction)
+
+def right(num, direction): #오른쪽 톱니바퀴 확인
+    if num > 3:
+        return
+    if s[num][6] != s[num-1][2]:
+        right(num+1, -direction)
+        s[num].rotate(direction)
+
+for i in range(K):
+    num = R[i][0] - 1
+    direction = R[i][1]
+    
+    left(num-1, -direction)
+    right(num+1, -direction)
+    s[num].rotate(direction)
+
+res = 0
+
+if s[0][0] == '1':
+    res += 1
+if s[1][0] == '1':
+    res += 2
+if s[2][0] == '1':
+    res += 4
+if s[3][0] == '1':
+    res += 8
+    
+print(res)
