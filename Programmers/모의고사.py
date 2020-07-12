b9bfbccacec30da212c9b42936086beb42ec2c9a
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-07-12 20:45:18 +0900

    브루트포스

--- Programmers/모의고사.py
+++ Programmers/모의고사.py
@@ -6,6 +6,7 @@ def solution(answers):
     s1 = [1, 2, 3, 4, 5]
     s2 = [2, 1, 2, 3, 2, 4, 2, 5]
     s3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
     res = [0, 0, 0]
     
     for i in range(len(answers)):
