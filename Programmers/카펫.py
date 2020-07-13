1ca028c21c399f8afda173881597ac511b959515
ahnsoheee <61968474+ahnsoheee@users.noreply.github.com>
2020-07-14 00:52:10 +0900

    브루트포스

--- Programmers/카펫.py
+++ Programmers/카펫.py
@@ -1,1 +1,10 @@
 # 카펫
+
+def solution(brown, yellow):
+    answer = []
+    
+    for i in range(1, int(yellow ** 0.5) + 1):
+        if (yellow % i == 0) and (2 * (i + yellow // i) + 4 == brown):
+            answer = [yellow//i+2, i+2]
+            
+    return answer
