bfb1cb0b6d4112196e518d28a12e0bd50c26f9b9
ahnsoheee <soheeeahn@gmail.com>
2021-02-11 21:16:35 +0900

    Greedy

---
+++ Baekjoon/1946.py
@@ -0,0 +1,21 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+  N = int(sys.stdin.readline()) 
+  s = []
+  result = 1
+
+  for _ in range(N):
+    s.append(list(map(int, sys.stdin.readline().split())))
+
+  s = sorted(s)
+  check = s[0][1]
+
+  for i in range(1, N):
+    if check > s[i][1]:
+      check = s[i][1]
+      result += 1
+
+  print(result)
