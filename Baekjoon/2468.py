b936ffe89dbcd4ad53172958469705e9459cd1ce
ahnsoheee <soheeeahn@gmail.com>
2021-02-20 00:09:45 +0900

    dfs, brute force

--- Baekjoon/2468.py
+++ Baekjoon/2468.py
@@ -1,4 +1,6 @@
 import sys, copy
+sys.setrecursionlimit(100000)
+
 
 N = int(sys.stdin.readline())
 s = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
@@ -6,15 +8,21 @@ s = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 for i in range(N):
   h = max(s[i])
 
+result = 1
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-def dfs(x, y):
+def dfs(x, y, tmp):
+  tmp[x][y] = 0
   for i in range(4):
-    nx, ny = x + dx[i], dy[i]
-    
+    nx, ny = x + dx[i], y + dy[i]
 
-for k in range(1, h-1):
+    if (0 <= nx < N) and (0 <= ny < N) and tmp[nx][ny] != 0:
+      dfs(nx, ny, tmp)
+
+for k in range(1, h+1):
   tmp = copy.deepcopy(s)
+  cnt = 0
+
   for i in range(N):
     for j in range(N):
       if tmp[i][j] <= k:
@@ -23,4 +31,9 @@ for k in range(1, h-1):
   for i in range(N):
     for j in range(N):
       if tmp[i][j] != 0:
-        dfs(i, j)
+        dfs(i, j, tmp)
+        cnt += 1
+  
+  result = max(result, cnt)
+
+print(result)
