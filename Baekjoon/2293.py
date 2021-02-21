9a856c884fa48a5acaf8b07ad169d3c823ac3b97
ahnsoheee <soheeeahn@gmail.com>
2021-02-22 00:03:39 +0900

    dp

--- Baekjoon/2293.py
+++ Baekjoon/2293.py
@@ -1,14 +1,14 @@
 import sys
 
-n, k = map(int, sys.stdin.readline.split())
+n, k = map(int, sys.stdin.readline().split())
 
-num = [int(sys.stdin.readline() for _ in range(n)]
-dp = [0 * k+1]
+num = [int(sys.stdin.readline()) for _ in range(n)]
+dp = [0 for _ in range(k+1)]
 dp[0] = 1
 
 for i in num:
   for j in range(i, k+1):
     if j-i >= 0:
-      dp[j] + dp[j-1]
+      dp[j] += dp[j-i]
 
 print(dp[k])
