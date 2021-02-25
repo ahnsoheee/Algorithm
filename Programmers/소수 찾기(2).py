58acd2875f92687c04018e281f04d822da38bef1
ahnsoheee <soheeeahn@gmail.com>
2021-02-25 23:49:51 +0900

    brute force

--- Programmers/소수 찾기(2).py
+++ Programmers/소수 찾기(2).py
@@ -1,7 +1,9 @@
+import math
+
 def solution(n):
     answer = 0
     arr = [1] * (n+1)
-    for i in range(2, int(n//2)+1):
+    for i in range(2, int(math.sqrt(n))+1):
         if arr[i]:
             for j in range(2*i, n+1, i):
                 arr[j] = 0
