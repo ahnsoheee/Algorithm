cd6cc6322bd5aa2250c0b495b242dae97ed63297
ahnsoheee <soheeeahn@gmail.com>
2021-02-25 22:19:13 +0900

    brute force

---
+++ Programmers/소수 찾기(2).py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+    arr = [1] * (n+1)
+    for i in range(2, int(n//2)+1):
+        if arr[i]:
+            for j in range(2*i, n+1, i):
+                arr[j] = 0
+    for i in range(2, n+1):
+        if arr[i]:
+            answer += 1
+    return answer
