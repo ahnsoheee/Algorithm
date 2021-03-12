63fe2fd72ab2a95da998373bd4c71222f4475ef2
ahnsoheee <soheeeahn@gmail.com>
2021-03-12 19:41:24 +0900

    dp

---
+++ Programmers/정수 삼각형.py
@@ -0,0 +1,15 @@
+def solution(triangle):
+    answer = 0
+    length = len(triangle)
+    
+    for i in range(1, length): 
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+                
+    answer = max(triangle[length-1])
+    return answer
