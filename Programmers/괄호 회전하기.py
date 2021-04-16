fd6cdb7e30303008eb8ce9e4e7cc76026f1f13f1
ahnsoheee <soheeeahn@gmail.com>
2021-04-16 18:26:29 +0900

    월코챌

---
+++ Programmers/괄호 회전하기.py
@@ -0,0 +1,26 @@
+def check(s):
+    stack = []
+    for w in s:
+        if w in ['[', '{', '(']:
+            stack.append(w)
+        elif w == ']' and (len(stack) == 0 or stack.pop() != '['):
+            return 0
+        elif w == '}' and (len(stack) == 0 or stack.pop() != '{'):
+            return 0
+        elif w == ')' and (len(stack) == 0 or stack.pop() != '('):
+            return 0
+    
+    if len(stack) != 0:
+        return 0
+
+    return 1
+
+        
+def solution(s):
+    answer = check(s)
+    for i in range(len(s)-1):
+        first = s[0]
+        s = s[1:] + s[0]
+        answer += check(s)
+        
+    return answer
