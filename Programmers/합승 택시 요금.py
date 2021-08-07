41ca34dca32d9d25e740426f6908b5a62533b4fd
ahnsoheee <soheeeahn@gmail.com>
2021-08-07 16:26:27 +0900

    다익스트라

---
+++ Programmers/합승 택시 요금.py
@@ -0,0 +1,39 @@
+import heapq
+
+INF = int(1e9)  
+
+def dijkstra(graph, start, end):
+    distances = [INF] * len(graph)
+    distances[start] = 0
+    queue = []
+    heapq.heappush(queue, (0, start))
+
+    while queue:
+        curr_d, curr_node = heapq.heappop(queue)
+
+        if distances[curr_node] < curr_d: continue
+
+        for next_d, next_node in graph[curr_node]:
+            distance = curr_d + next_d
+
+            if distance < distances[next_node]:
+                distances[next_node] = distance
+                heapq.heappush(queue, (distance, next_node))
+        
+    return distances[end]
+
+def solution(n, s, a, b, fares):   
+    graph = [[] for _ in range(n)]
+ 
+    answer = dijkstra(graph, s-1, a-1) + dijkstra(graph, s-1, b-1)
+    answer = INF
+    for i, j, cost in fares:
+        graph[i-1].append((cost, j-1))
+        graph[j-1].append((cost, i-1))
+    
+    for i in range(n):
+        answer = min(answer, dijkstra(graph, s-1, i) + dijkstra(graph, i, a-1) + dijkstra(graph, i, b-1))
+  
+    return answer
+
+print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
