1e1ce7be86f3ee1193cbdfd41f4cb2c294496e88
ahnsoheee <soheeeahn@gmail.com>
2021-08-07 16:27:22 +0900

    다익스트라

--- Programmers/합승 택시 요금.py
+++ Programmers/합승 택시 요금.py
@@ -35,5 +35,3 @@ def solution(n, s, a, b, fares):
         answer = min(answer, dijkstra(graph, s-1, i) + dijkstra(graph, i, a-1) + dijkstra(graph, i, b-1))
   
     return answer
-
-print(solution(6, 4, 6, 2, [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]))
