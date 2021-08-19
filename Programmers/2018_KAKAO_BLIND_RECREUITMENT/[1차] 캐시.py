7b7d9bf67e3b1e0fe9caa2b8226e62c86c90082b
ahnsoheee <soheeeahn@gmail.com>
2021-08-19 13:27:08 +0900

    2018_KAKAO

---
+++ Programmers/2018_KAKAO_BLIND_RECREUITMENT/[1차] 캐시.py
@@ -0,0 +1,22 @@
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    
+    if cacheSize != 0:
+        for city in cities:
+            city = city.lower()
+        
+            if city in cache:
+                cache.remove(city)
+                cache.append(city)
+                answer += 1
+            else:
+                if len(cache) >= cacheSize:
+                    cache.pop(0)
+                    
+                cache.append(city)
+                answer += 5
+    else:
+        answer += len(cities) * 5
+                
+    return answer
