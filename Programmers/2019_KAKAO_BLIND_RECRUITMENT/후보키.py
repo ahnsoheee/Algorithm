42662f3ae4ae94694ca91e51b0d3dfa3720636ac
ahnsoheee <soheeeahn@gmail.com>
2021-11-26 00:20:28 +0900

    2019_KAKAO

--- Programmers/2019_KAKAO_BLIND_RECRUITMENT/후보키.py
+++ Programmers/2019_KAKAO_BLIND_RECRUITMENT/후보키.py
@@ -35,7 +35,3 @@ def solution(relation):
                 unique.append(combination)
 
     return len(unique)
-
-
-solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], [
-         "400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]])
